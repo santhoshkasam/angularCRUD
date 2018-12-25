18a85d0fb0a842fea1cfedc068a7965cd1bb26fd
Santhosh Kasam <santhosh.kasam@thomsonreuters.com>
2018-12-25 10:05:25 +0530

    ngx-datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 import { AppComponent } from './app.component';
 import { ListEmployeesComponent } from './employees/list-employees.component';
@@ -23,6 +24,7 @@ const appRoutes: Routes =
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
+    BsDatepickerModule.forRoot(),
     FormsModule
   ],
   providers: [],
