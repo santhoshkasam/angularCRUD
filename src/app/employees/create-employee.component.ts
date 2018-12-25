bc1065a606a5460c650b9d466f49a6c1a95137dc
Santhosh Kasam <santhosh.kasam@thomsonreuters.com>
2018-12-25 22:11:06 +0530

    Radio button validation

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Department } from '../models/department.model';
 import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker/public_api';
+import { Employee } from '../models/employee.model';
 
 @Component({
   selector: 'app-create-employee',
@@ -17,14 +18,27 @@ export class CreateEmployeeComponent implements OnInit {
     { id: 3, name: 'IT' },
     { id: 4, name: 'Payroll' }
   ];
+  employee: Employee = {
+    id: null,
+    name: null,
+    gender: null,
+    email: null,
+    phoneNumber: null,
+    contactPreference: null,
+    dateOfBirth: null,
+    department: null,
+    isActive: null,
+    photoPath: null
+  };
+
   constructor() {
     this.datePickerConfig = Object.assign({}, { containerClass: 'theme-dark-blue' });
   }
 
   ngOnInit() {
   }
-  saveEmployee(empForm: NgForm): void {
-    console.log(empForm.value);
+  saveEmployee(emp: Employee): void {
+    console.log(emp);
   }
   togglePhotoPreview(): void {
     this.previewPhoto = !this.previewPhoto;
